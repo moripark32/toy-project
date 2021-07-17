f8471fe3f8759ea5b0dc23fd02f495c0ae3030c5
moripark32 <moripark32@gmail.com>
2021-07-17 11:37:58 +0900

    fix:Responsive

--- src/Pages/Components/Nav/Components/Stack.js
+++ src/Pages/Components/Nav/Components/Stack.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import '../Components/DropBar.scss';
+import '../Components/Stack.scss';
 
 const Stack = () => {
   return (
-    <div className="function">
-      hello
+    <div className="stack">
       <FontAwesomeIcon icon={faSearch} />
+      <a className="loginSignup">회원가입/로그인</a>
+      <div className="service">기업서비스</div>
     </div>
   );
 };
