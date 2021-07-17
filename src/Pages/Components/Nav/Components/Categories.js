d19e9bc375587a8a052ef622a619d1e1918c9004
moripark32 <moripark32@gmail.com>
2021-07-17 15:25:08 +0900

    add:responsive

--- src/Pages/Components/Nav/Components/Categories.js
+++ src/Pages/Components/Nav/Components/Categories.js
@@ -8,23 +8,11 @@ const Categories = props => {
   const [mainCategories, setMainCategories] = useState([]);
   const { closeHover } = props;
 
-  // useEffect(() => {
-  //   fetch('/data/categories.json')
-  //     .then(res => res.json())
-  //     .then(list => {
-  //       setCategoriesList(list.CATEGORIES);
-  //     })
-  //     .catch(err => console.log(err));
-  // }, []);
-
-  // Promise.all([]);
-
   useEffect(() => {
     Promise.all([
       fetch('/data/categories.json'),
       fetch('/data/mainCategories.json'),
     ])
-      // .then(([res1, res2]) => [res1.json(), res2.json()])
       .then(([res1, res2]) => Promise.all([res1.json(), res2.json()]))
       .then(([categories, main]) => {
         console.log({ categories, main });
